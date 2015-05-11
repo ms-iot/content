1763e8f0935c764acc802b4226c5e52962e322de
Nick Lathe <nicklathe@icloud.com>
2015-05-11 09:54:42 -0700

    4 step process works. Finished jQuery to color active step black

--- js/app.js
+++ js/app.js
@@ -2,16 +2,13 @@
     $('img:not(.nr)').addClass('img-responsive');
 
     var currentUrl = $(location).attr('pathname');
-    console.log(currentUrl);
 
     var urlArray = $('.steps').parent();
-    // .attr('href');
 
     for(var i = 0; i < urlArray.length; i++) {
         if($(urlArray[i]).attr('href') == currentUrl) {
-            console.log($(urlArray[i]).attr('href'));
-            $(urlArray[i]).removeClass('inactive').addClass('active');
-        }
+            $(urlArray[i]).children().removeClass('inactive').addClass('active');
+        };
     };
 
 });
