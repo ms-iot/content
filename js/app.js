162bd5f8b839f1c7e3a94f7cc9139f8aa5479c77
Nick Lathe <nicklathe@icloud.com>
2015-05-15 14:10:46 -0700

    Refactored JS

--- js/app.js
+++ js/app.js
@@ -1,14 +1,4 @@
 ﻿$(function () {
     $('img:not(.nr)').addClass('img-responsive');
-
-    var currentUrl = $(location).attr('pathname');
-
-    var urlArray = $('.steps').parent();
-
-    for(var i = 0; i < urlArray.length; i++) {
-        if($(urlArray[i]).attr('href') == currentUrl) {
-            $(urlArray[i]).children().removeClass('inactive').addClass('active');
-        };
-    };
-
+    $("a[href='" + window.location.pathname + "'] h3").toggleClass('active');
 });
