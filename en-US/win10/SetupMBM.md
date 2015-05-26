87d9f1629cd89bf864521242c5156392273125a5
Nick Lathe <nicklathe@icloud.com>
2015-05-26 15:49:53 -0700

    Added redirect pages, and worked on JS redirect script

--- en-US/win10/SetupMBM.md
+++ en-US/win10/SetupMBM.md
@@ -3,7 +3,6 @@ layout: default
 title: SetupBoard
 permalink: /en-US/win10/SetupMBM.htm
 lang: en-US
-redirect_from: /content/win10/SetupMBM.htm
 ---
 
 #Get Started
--- js/app.js
+++ js/app.js
@@ -2,4 +2,11 @@
     $('img:not(.nr)').addClass('img-responsive');
     var filename = window.location.pathname.split('/').pop();
     $("a[href*='" + filename + "'] h3").toggleClass('active');
+
+    var url = window.location.pathname;
+
+    if(! url.includes('en-US')){
+        var newUrl = url.replace('content', 'content/en-US');
+        window.location.replace(newUrl);
+    };
 });
